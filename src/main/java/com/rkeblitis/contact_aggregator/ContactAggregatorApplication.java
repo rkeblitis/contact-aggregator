5ddfde25eded21cebb7e86eebd49ab57872bd01d
Rachelle <rkeblitis@gmail.com>
2026-06-06 16:28:07 -0400

    initial project scaffold

---
+++ src/main/java/com/rkeblitis/contact_aggregator/ContactAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.rkeblitis.contact_aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactAggregatorApplication.class, args);
+	}
+
+}
